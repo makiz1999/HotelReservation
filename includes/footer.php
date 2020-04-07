0f6e38a93d1abd262e3fdb799842b92dfb9d79b5
Max <49218301+makiz1999@users.noreply.github.com>
2020-04-07 18:31:01 +0300

    Change project to PHP / Add footer, header

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<div class="footer">
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h1 class="logo-text">HotelReservation</h1>
+            <p>
+                Hotel Reservation web app is the project for Software Engineering class
+            </p>
+            <div class="contact">
+                <span><i class="fa fa-phone aria-hidden="true""></i>  &nbsp; 000-000-000</span>
+                <span><i class="fa fa-envelope"></i>  &nbsp; info@hotelreservation.com</span>
+            </div>
+            <div class="socials">
+                <a href="#"><i class="fa fa-facebook"></i> </a>
+                <a href="#"><i class="fa fa-instagram"></i> </a>
+                <a href="#"><i class="fa fa-twitter"></i> </a>
+                <a href="#"><i class="fa fa-youtube"></i> </a>
+            </div>
+        </div>
+
+        <div class="footer-section links">
+            <h2>Quick Links</h2>
+            <br>
+            <ul>
+                <a href="#"><li>Information</li></a>
+                <a href="#"><li>Terms</li></a>
+                <a href="#"><li>Contact</li></a>
+            </ul>
+        </div>
+
+        <div class="footer-section contact-form">
+            <h2>Contact us</h2>
+            <br>
+            <form action="home.php" method="post">
+                <input type="email" name="email" class="text-input contact-input" placeholder="Your email address">
+                <textarea name="message" class="text-input contact-input"  placeholder="Your message"></textarea>
+                <button type="submit" class="btn btn-big">
+                    <i class="fa fa-envelope"></i>
+                    Send
+                </button>
+            </form>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        &copy; Group1 Project
+    </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Hotel Reservation | Main Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+<header>
+    <div class = "container">
+        <div id="company-name">
+            <h1>Hotel Reservation</h1>
+        </div>
+        <nav>
+            <ul>
+                <li class="current"><a href="home.php">Home</a></li>
+                <li><a href="#">About</a> </li>
+                <li><a href="#">Login</a> </li>
+            </ul>
+        </nav>
+    </div>
+</header>
