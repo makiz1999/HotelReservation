797386bc407aa428ee61f26e51d8ec91e28be63c
Nadiya Navas <nadiyanavas77@gmail.com>
2020-04-21 13:03:33 -0500

    Updates to about and footer

--- about.php
+++ about.php
@@ -2,9 +2,18 @@
 include ('includes/header.php')
 ?>
 
-<section id="about-content">
-  <div class="purpose">
-    <p> This is a project of COMP3300. </p>
+<section id="about">
+  <div class="about-content">
+      <h1> This is a project of COMP3300 </h1>
+      <p> We are dedicated to finding you the best hotels at the cheapest price to make your experience the best it can be.</p>
+  </div>
+  <div class="about-contact">
+      <p>
+          For Any Inquiries Please Contact Us
+      </p>
+          <span><i class="fa fa-phone aria-hidden="true""></i>  &nbsp; 000-000-000</span>
+          <br>
+          <span><i class="fa fa-envelope"></i>  &nbsp; info@hotelreservation.com</span>
   </div>
 </section>
 
--- includes/footer.php
+++ includes/footer.php
@@ -1,51 +1,42 @@
 <div class="footer">
     <div class="footer-content">
-        <div class="footer-section about">
+      <div class="footer-section about">
+        <p>
+            Visit the About Page for Contact Information.
+        </p>
+          <div class="contact">
+              <span><i class="fa fa-mobile aria-hidden="true""></i>  &nbsp; Available now in App Store</span>
+              <br>
+              <span><i class="fa fa-bed aria-hidden="true""></i>  &nbsp; We Operate Nationwide</span>
+              <br>
+          </div>
+          <br>
           <p>
-              For Any Inquiries Please Contact Us
+              Find and Follow Us On:
           </p>
-            <div class="contact">
-                <span><i class="fa fa-phone aria-hidden="true""></i>  &nbsp; 000-000-000</span>
-                <br>
-                <span><i class="fa fa-envelope"></i>  &nbsp; info@hotelreservation.com</span>
-            </div>
-            <br>
-            <p>
-                Find and Follow Us On:
-            </p>
-            <div class="socials">
-                <a href="#"><i class="fa fa-facebook"></i> </a>
-                <a href="#"><i class="fa fa-instagram"></i> </a>
-                <a href="#"><i class="fa fa-twitter"></i> </a>
-                <a href="#"><i class="fa fa-youtube"></i> </a>
-            </div>
-        </div>
+          <div class="socials">
+              <a href="#"><i class="fa fa-facebook"></i> </a>
+              <a href="#"><i class="fa fa-instagram"></i> </a>
+              <a href="#"><i class="fa fa-twitter"></i> </a>
+              <a href="#"><i class="fa fa-youtube"></i> </a>
+          </div>
+      </div>
+      <div class="footer-section contact-form">
+          <p>
+            Let us know about your experience!
+          </p>
+          <form action="home.php" method="post">
+              <input type="email" name="email" class="text-input contact-input" placeholder="Your email address">
+              <br>
+              <textarea name="message" class="text-input contact-input"  placeholder="Your message"></textarea>
+              <br><br>
+              <button type="submit" class="btn btn-big">
+                  <i class="fa fa-envelope"></i>
+                  Send
+              </button>
+          </form>
+      </div>
 
-        <!--<div class="footer-section links">
-            <h2>Quick Links</h2>
-            <br>
-            <ul>
-                <a href="#"><li>Information</li></a>
-                <a href="#"><li>Terms</li></a>
-                <a href="#"><li>Contact</li></a>
-            </ul>
-        </div>-->
-
-        <div class="footer-section contact-form">
-            <p>
-              Let us know about your experience!
-            </p>
-            <form action="home.php" method="post">
-                <input type="email" name="email" class="text-input contact-input" placeholder="Your email address">
-                <br>
-                <textarea name="message" class="text-input contact-input"  placeholder="Your message"></textarea>
-                <br><br>
-                <button type="submit" class="btn btn-big">
-                    <i class="fa fa-envelope"></i>
-                    Send
-                </button>
-            </form>
-        </div>
     </div>
     <div class="footer-bottom">
         &copy; Group1 Project
